922292898e1cdf328dedcb73584734d2c5e68ad2
Sergio-Paucara <Sergio.Paucara@coxautoinc.com>
2024-12-10 09:40:26 -0400

    fix events

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,4 +7,5 @@ export default defineConfig({
   build: {
     outDir: 'build', // Asegúrate de que el directorio de salida sea 'build'
   },
-})
+  base: '/events/', // Cambia esto para que coincida con el subdirectorio
+});
